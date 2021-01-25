543479779fec5c95a2c70720878f2dadaa54c56a
baregamyan <baregamyan@gmail.com>
2021-01-25 20:19:02 +0300

    feat: global update

--- src/js/modules/home-slider.js
+++ src/js/modules/home-slider.js
@@ -2,7 +2,7 @@ import Swiper, { Autoplay, Navigation, Pagination, EffectFade } from 'swiper';
 
 Swiper.use([Navigation, Pagination, Autoplay, EffectFade]);
 
-const homeSlider = new Swiper('.home__slider', {
+const homeSlider = new Swiper('.home__slider-container', {
   loop: true,
   spaceBetween: 100,
   autoplay: {
